93bd020ae86632da3a820d2cd945602c4bc24f10
McKay Ward <mckaykward@gmail.com>
2016-10-04 15:36:03 -0500

    use flags for api key, but default to env variable

--- slack_bot.go
+++ slack_bot.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/nlopes/slack"
 	"os"
 )
 
 func main() {
-	key := os.Getenv("SLACK_BOT_API_KEY")
-	api := slack.New(key)
+
+	var key = flag.String("k", os.Getenv("SLACK_BOT_API_KEY"), "The Slack API key for the bot")
+	flag.Parse()
+
+	if *key == "" {
+		fmt.Println("You must supply an API key via the -k flag or by setting the SLACK_BOT_API_KEY environment variable")
+		os.Exit(1)
+	}
+
+	api := slack.New(*key)
 
 	users, err := api.GetUsers()
 
